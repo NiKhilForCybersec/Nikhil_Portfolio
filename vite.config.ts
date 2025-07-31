befb54659a98ae06a15a80382631a189aca6abe7
NiKhilForCybersec <nikhileswarreddy.c@gmail.com>
2025-07-30 20:18:28 -0400

    Css files

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Nikhil-portfolio/',
   plugins: [react()],
-  build: {
-    outDir: '../', // Build to parent directory (root of repo)
-    emptyOutDir: false, // Don't empty the entire parent directory
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
